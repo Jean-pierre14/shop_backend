0c32a433674c8382bc5c7e166e75154f9b987164
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-06-30 23:45:28 +0300

    add: APIs Done

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -3,43 +3,86 @@ import DataModel from "./../models/index.model.js";
 export const createData = async (req, res) => {
   const { name, quantity, size } = req.body;
 
-  const data = await DataModel.create({
-    name,
-    quantity,
-    size,
-  });
+  try {
+    const data = await DataModel.create({
+      name,
+      quantity,
+      size,
+    });
 
-  if (data) {
-    res.json({ msg: "data register " + data }).status(201);
-  } else {
-    res.json({ msg: "Error db" }).status(500);
+    if (data) {
+      res.status(201).json({ msg: "Data registered: " + data });
+    } else {
+      res.status(500).json({ msg: "Error in the database" });
+    }
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    res.status(500).json({ msg: "Error in the server" });
   }
 };
 
 export const getAll = async (req, res) => {
   try {
     const data = await DataModel.find({});
-    res.json(data).status(201);
+    res.status(200).json(data);
   } catch (err) {
     console.log(`Error: ${err.message}`);
+    res.status(500).json({ msg: "Error in the server" });
   }
 };
 
 export const getOne = async (req, res) => {
   let dataId = req.params.id;
-  await DataModel.findById(dataId)
-    .then((data) => res.json({ msg: data }).status(201))
-    .catch((error) => res.status(500).json({ msg: error.message }));
+  try {
+    const data = await DataModel.findById(dataId);
+    if (data) {
+      res.status(200).json({ msg: data });
+    } else {
+      res.status(404).json({ msg: "Data not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
 export const updateData = async (req, res) => {
-  res.json({ message: "Update" }).status(200);
+  let dataId = req.params.id;
+  const { name, quantity, size } = req.body;
+
+  try {
+    const data = await DataModel.findByIdAndUpdate(
+      dataId,
+      {
+        name,
+        quantity,
+        size,
+      },
+      { new: true }
+    );
+
+    if (data) {
+      res.status(200).json({ msg: "Data updated successfully" });
+    } else {
+      res.status(404).json({ msg: "Data not found" });
+    }
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    res.status(500).json({ msg: "Error in the server" });
+  }
 };
 
 export const deleteOne = async (req, res) => {
   let dataId = req.params.id;
 
-  await DataModel.findOneAndDelete(dataId)
-    .then((data) => res.json(data).status(201))
-    .catch((err) => console.log(err));
+  try {
+    const data = await DataModel.findByIdAndDelete(dataId);
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ msg: "Data not found" });
+    }
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    res.status(500).json({ msg: "Error in the server" });
+  }
 };
