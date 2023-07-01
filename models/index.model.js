25ee2d5b07d573bc73188665ff1d912cceb8dce5
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-07-01 03:07:43 +0300

    add: git and model config

--- models/index.model.js
+++ models/index.model.js
@@ -14,6 +14,10 @@ const dataSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    // user_id: {
+    //   type: String,
+    //   required: true,
+    // },
   },
   { timestamps: true }
 );
